1f4c89d740ea3efc20a2ed9efbffe8c7107b8210
leejune22 <kugelsch2022@gmail.com>
2022-08-09 15:51:32 +0900

    여러가지 명령어 추가 연습

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -5,20 +5,36 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("==프로그램 시작==");
-		
+
 		Scanner sc = new Scanner(System.in);
 		
+		int writenum = 0;
+
 		while (true) {
+			
 			System.out.printf("명령어) ");
 			String cmd = sc.nextLine();
-			
+
 			if (cmd.equals("exit")) {
 				break;
+			} else if (cmd.equals("article list")) {
+				System.out.println("게시글이 없습니다");
+			} else if (cmd.equals("article write")) {
+				
+				System.out.printf("제목: ");
+				String title = sc.nextLine();
+				System.out.printf("내용: ");
+				String content = sc.nextLine();
+				writenum++;
+				System.out.printf("%d번 글이 저장되었습니다./n" , writenum);
+
+			} else {
+				System.out.println("존재하지 않는 명령어입니다");
 			}
 		}
-		
+
 		sc.close();
-		
-		System.out.println("==프로그램 끝==");
+
+		System.out.println("==프로그램 종료==");
 	}
 }
