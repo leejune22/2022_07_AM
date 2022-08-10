4cccf1ce1d2998d6595fbb560056623f4779632e
leejune22 <kugelsch2022@gmail.com>
2022-08-10 15:48:24 +0900

    게시글 배열 저장 + 숙제

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -1,5 +1,7 @@
 package com.KoreaIT.java.AM;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -7,29 +9,54 @@ public class Main {
 		System.out.println("==프로그램 시작==");
 
 		Scanner sc = new Scanner(System.in);
-		
-		int writenum = 0;
+
+		List<Article> articles = new ArrayList<>();
+
+		int lastArticleId = 0;
 
 		while (true) {
-			
-			System.out.printf("명령어) ");
-			String cmd = sc.nextLine();
 
-			if (cmd.equals("exit")) {
-				break;
-			} else if (cmd.equals("article list")) {
-				System.out.println("게시글이 없습니다");
+			System.out.printf("명령어) ");
+			String cmd = sc.nextLine().trim();
+
+			if (cmd.equals("article list")) {
+				if (articles.size() == 0) {
+					System.out.println("게시글이 없습니다");
+					continue;
+				}
+
+				else {
+					System.out.println("번호/제목");
+					for (int i = articles.size() - 1; i >= 0; i--) {
+						Article article = articles.get(i);
+						System.out.printf("%d / %s\n", article.id, article.title);
+					}
+				}
 			} else if (cmd.equals("article write")) {
-				
+				int id = lastArticleId + 1;
 				System.out.printf("제목: ");
 				String title = sc.nextLine();
 				System.out.printf("내용: ");
 				String content = sc.nextLine();
-				writenum++;
-				System.out.printf("%d번 글이 저장되었습니다./n" , writenum);
+				lastArticleId++;
 
-			} else {
+				Article article = new Article(id, title, content);
+				articles.add(article);
+
+				System.out.printf("%d번 글이 생성되었습니다.\n", id);
+
+			}
+
+			else if (cmd.startsWith("article detail ")) {
+				System.out.println("");
+			}
+
+			else {
 				System.out.println("존재하지 않는 명령어입니다");
+			}
+
+			if (cmd.equals("exit")) {
+				break;
 			}
 		}
 
@@ -37,4 +64,17 @@ public class Main {
 
 		System.out.println("==프로그램 종료==");
 	}
+}
+
+class Article {
+	int id;
+	String title;
+	String content;
+
+	Article(int id, String title, String content) {
+		this.id = id;
+		this.title = title;
+		this.content = content;
+	}
+
 }
